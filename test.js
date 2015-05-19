26b7dce0a178038e333b561b4526ac84ac230476
Jack.Rans <jwrans@gmail.com>
2015-05-19 16:17:30 +0100

    Added test script changes

--- test.js
+++ test.js
@@ -1,62 +1,42 @@
-test("this bit should describe, to a human, what the test is for", function() {
-    equal( 1, 1);
-});
+//
+// test("testing on header", function() {
+//     var iframe = document.getElementById('if-index');
+//
+//     var target = iframe.contentDocument || iframe.contentWindow.document;
+//
+//     var initial = target.getElementById('heading');
+//
+//     var upper = getComputedStyle(initial,null).textTransform;
+//
+//     // if you have a property like text-transform must use textTransform instead
+//
+//     equal(initial.innerHTML, 'The test of both worlds', 'header text is right and exists');
+//
+//     equal(upper, "uppercase", 'uppercase been used');
+// });
 
-
-test("testing on header", function() {
-    var iframe = document.getElementById('if-index');
-
-    var target = iframe.contentDocument || iframe.contentWindow.document;
-
-    var initial = target.getElementById('heading');
-
-    var upper = getComputedStyle(initial,null).textTransform;
-
-    // if you have a property like text-transform must use textTransform instead
-
-    equal(initial.innerHTML, 'The test of both worlds', 'header text is right and exists');
-
-    equal(upper, "uppercase", 'uppercase been used');
-});
-
-
-
-test("add function", function() {
-    var x=Math.floor(Math.random()*1000);
-    var y=Math.floor(Math.random()*1000);
-    equal(add(x,y), x+y, 'it works');
-    equal(add(10,2),12)
-
-
-});
-
-test("Is there an image on the page", function(){
+test("Divs exist and in correct order", function(){
 var iframe = document.getElementById('if-index');
 var target = iframe.contentDocument || iframe.contentWindow.document;
-var testimage = target.getElementsByTagName("img")[0].alt;
-
-notEqual(testimage, "", "yeah");
-
+var first = target.getElementsByTagName("div")[0].id;
+var second = target.getElementsByTagName("div")[1].id;
+var box1 = target.getElementsByTagName("div")[2].id;
+var box2 = target.getElementsByTagName("div")[3].id;
+var box3 = target.getElementsByTagName("div")[4].id;
+var last = target.getElementsByTagName("div")[5].id;
+equal(first, "header", "header div present and on top");
+equal(second, "navigation", "navigation div present and on top");
+equal(box1, "hashtags", "header div present and on top");
+equal(box2, "users", "navigation div present and on top");
+equal(box3, "about", "header div present and on top");
+equal(last, "footer", "header div present and on top");
 });
 
-test("check that a tiger shows up", function() {
+test("check logo present", function() {
     var iframe= document.getElementById('if-index');
     var target= iframe.contentDocument || iframe.contentWindow.document;
-    var initial= target.getElementById('tiger').src;
+    var logo= target.getElementById('logo').alt;
 
-    equal(initial, 'http://www.hdwallpapersimages.com/wp-content/uploads/2014/01/Winter-Tiger-Wild-Cat-Images.jpg', 'tiger match')
+    equal(logo, 'InstaCognito logo', 'logo image present and given alternative text name')
 
 });
-
-
-
-
-
-
-
-
-
-
-
-// test("Has the CSS loaded?", function() {
-// } )
