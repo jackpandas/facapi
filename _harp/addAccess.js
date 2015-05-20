901468d840fe9f8b5fe35cb8c7650d145e89866b
Jack.Rans <jwrans@gmail.com>
2015-05-20 17:55:08 +0100

    updated accAccess to fix broken stuff and revert back to working, slight change to tests

--- _harp/addAccess.js
+++ _harp/addAccess.js
@@ -4,27 +4,25 @@ var accesstoken="";
 
 function loadtag(){
   var tag = document.getElementById('hashtag1').value;
-  var section='hashtags';
+  // var section='hashtags';
   if (tag){//checking to see if empty
     var apiRequest= "https://api.instagram.com/v1/tags/" + tag + "/media/recent?access_token=" + accesstoken + "&callback=callbackFunction";
     var apiscript=document.createElement('script');
     apiscript.setAttribute('src', apiRequest);
     document.body.appendChild(apiscript);
   }//end if empty
-  return section;
 };//end load
-
-function loaduser(){
-  var user = document.getElementById('user1').value;
-  var section='users';
-  if (user){//checking to see if empty
-    var apiRequest= "https://api.instagram.com/v1/users/" + user + "/media/recent?access_token=" + accesstoken + "&callback=callbackFunction";
-    var apiscript=document.createElement('script');
-    apiscript.setAttribute('src', apiRequest);
-    document.body.appendChild(apiscript);
-  }//end if empty
-  return section;
-};//end load
+// 
+// function loaduser(){
+//   var user = document.getElementById('user1').value;
+//   // var section='users';
+//   if (user){//checking to see if empty
+//     var apiRequest= "https://api.instagram.com/v1/users/" + user + "/media/recent?access_token=" + accesstoken + "&callback=callbackFunction";
+//     var apiscript=document.createElement('script');
+//     apiscript.setAttribute('src', apiRequest);
+//     document.body.appendChild(apiscript);
+//   }//end if empty
+// };//end load
 
 
 function callbackFunction(dataReturned){
@@ -32,13 +30,13 @@ function callbackFunction(dataReturned){
   for (var i=0;i<data.length;i++){
     var content = data[i];
     var div = document.createElement('div');
-    var image=document.createElement('img');
     var title=document.createElement('p');
     title.innerHTML=content.caption.text;
+    var image=document.createElement('img');
     image.setAttribute('src', content.images.standard_resolution.url);
     image.setAttribute('alt', content.caption.text);
     div.appendChild(title);
     div.appendChild(image);
-    document.getElementById(section).appendChild(div);
+    document.getElementById("hashtags").appendChild(div);
   };
 };
