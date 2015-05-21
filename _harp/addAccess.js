0ce0d3859e6cef151a62fc9fcd883a3e7cc4efd2
Jack McDonald <jack@jackmcdonald.org>
2015-05-21 17:43:12 +0100

    Implemented sort function on returned media array because Jack Rans is a mathematics wizard

--- _harp/addAccess.js
+++ _harp/addAccess.js
@@ -2,6 +2,8 @@
 
 var usersImageArray = []; // PUTINERHERHERHERHERHERHERHERHEHEHE
 
+
+
 function loadtag(){
   var tag = document.getElementById('hashtag1').value;
   // var section='hashtags';
@@ -18,7 +20,7 @@ function loaduser(userId){
   var user = userId;
   // var section='users';
   if (user){//checking to see if empty
-    var apiRequest= "https://api.instagram.com/v1/users/" + user + "/media/recent?access_token=" + accesstoken + "&callback=callbackFunction";
+    var apiRequest= "https://api.instagram.com/v1/users/" + user + "/media/recent?access_token=" + accesstoken + "&callback=fillRecentArray";
     var apiscript=document.createElement('script');
     apiscript.setAttribute('src', apiRequest);
     document.body.appendChild(apiscript);
@@ -27,8 +29,8 @@ function loaduser(userId){
 
 
 function callbackFunction(dataReturned){
-  var data=dataReturned.data;
-  console.log(dataReturned.data);
+  var data=dataReturned;
+  //console.log(dataReturned.data);
   for (var i=0;i<data.length;i++){
     
     var content = data[i];
@@ -55,8 +57,9 @@ function callbackFunction(dataReturned){
   };
 };
 
+
 function loadUserArray(){
-var usersarray = ['jackpandas','jack.rans','taylorswift','kimkardashian','zooeydeschanel'];
+var usersarray = ['jack.rans','taylorswift','kimkardashian','zooeydeschanel','jackpandas'];
 
   for (var j = 0; j< usersarray.length;j++){
         var userEncoded = encodeURIComponent( usersarray[j] );
@@ -66,7 +69,7 @@ var usersarray = ['jackpandas','jack.rans','taylorswift','kimkardashian','zooeyd
         apiscript.setAttribute('src', apiRequest);
         document.body.appendChild(apiscript);
   }
-
+    console.log(usersImageArray);
 };//end load
 
 
@@ -75,8 +78,8 @@ function loadUserId(){
   var userEncoded = encodeURIComponent(user);
   // var section='hashtags';
   if (user){//checking to see if empty
-    var apiRequest= "https://api.instagram.com/v1/users/search?q=" + '"' + userEncoded + '"' + "&access_token=" + accesstoken + "&callback=callbackGetUser";
-      console.log(apiRequest);
+    var apiRequest= "https://api.instagram.com/v1/users/search?q=" + '"' + userEncoded + '"' + "&access_token=" + accesstoken + "&callback=fillRecentArray";
+      //console.log(apiRequest);
     var apiscript=document.createElement('script');
     apiscript.setAttribute('src', apiRequest);
     document.body.appendChild(apiscript);
@@ -93,3 +96,132 @@ function callbackGetUser (dataReturned){
     // return userId;
 
 }
+
+
+//This function fills the usersImageArray with recent media
+function fillRecentArray(recentMediaData) {
+    var data = recentMediaData.data;
+    for (var k=0; k<data.length;k++) {
+        var content = data[k];
+        usersImageArray.push(content);
+    }
+    //console.log(usersImageArray);
+
+}
+//This function parseInts every timestamp in the array (failed)
+//function timeStampParse(array) {
+//    var that = [];
+//    for (var p = 0; p < array.length;p++){
+//        var time = parseInt(array[p].created_time);
+//        that[p].created_time = time;
+//    } return that
+//    
+//}
+
+
+//This function sorts the usersImageArray by timestamp of individual pictures
+//function sortArray(array) {
+//    var len = array.length,
+//        value,
+//        i,
+//        j;
+//    for (i=0; i < len; i++) {
+//        value = parseInt(array[i].created_time);
+//        for (j=i-1; j > -1 && array[j] > value; j--) {
+//            parseInt(array[j+1]).created_time = parseInt(array[j].created_time);
+//   }
+//       parseInt(array[j+1]).created_time = value;
+//    
+//         
+//        }
+//        return array;
+//}
+
+
+function pivotSort (array){ //quicksort of array in highlest to lowest
+    var n =array.length;
+    if (n<2){
+        return array;
+    }
+    else if (n===2){
+        if (array[0][1]>array[1][1]){return array;}
+        else {return [array[1],array[0]]};
+    }
+    else {
+        var lower=[];
+        var equal=[];
+        var higher=[];
+        var pivot =Math.floor(n/2);
+        var pivValue=array[pivot][1];
+        for (var j=0;j<n;j++){
+            if (array[j][1]<pivValue){lower.push(array[j]);}
+            else if (array[j][1]===pivValue){equal.push(array[j]);}
+            else {higher.push(array[j]);}
+        };
+        return pivotSort(higher).concat(equal.concat(pivotSort(lower)));
+    };
+};
+
+
+
+
+
+function sortbytime (array){
+    var n= array.length; //number of objects in array
+    var sortArray=[];
+    for (var i=0;i<n;i++){
+        sortArray[i]=[array[i],parseInt(array[i].created_time)];
+    };
+    var preresult=pivotSort(sortArray);
+    var result =[];
+    for (var k=0;k<n;k++){
+        result[k]=preresult[k][0];
+    };
+    return result;
+    
+
+    
+};
+    
+    
+    
+
+
+
+
+
+//function insertionSort(items) {
+//
+//    var len     = items.length,     // number of items in the array
+//        value,                      // the value currently being compared
+//        i,                          // index into unsorted section
+//        j;                          // index into sorted section
+//    
+//    for (i=0; i < len; i++) {
+//    
+//        // store the current value because it may shift later
+//        value = items[i];
+//        
+//        /*
+//         * Whenever the value in the sorted section is greater than the value
+//         * in the unsorted section, shift all items in the sorted section over
+//         * by one. This creates space in which to insert the value.
+//         */
+//        for (j=i-1; j > -1 && items[j] > value; j--) {
+//            items[j+1] = items[j];
+//        }
+//
+//        items[j+1] = value;
+//    }
+//    
+//    return items;
+//}
+//
+////console.log(parseInt(usersImageArray[0].created_time))
+//
+
+
+
+
+
+
